e72d234712d59da662d85a90683d5d04994cbd8d
amathziah <amathziah@amathziahs-MacBook-Pro.local>
2024-10-25 15:04:24 +0530

    Your commit message here

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import io from "socket.io-client";
 import { useNavigate } from "react-router-dom";
